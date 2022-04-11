c2776cdbc1a6258a5736a87fd7f36862e1be3b9c
Dragindor <86678395+Dragindor@users.noreply.github.com>
2022-04-11 19:10:06 +0300

    I replaced the login/register button with login button as I didnt need it and make an if statment so the button wont be there if you havend logged in.

---
+++ GameReviewSite/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using GameReviewSite.Core.Contracts;
+using GameReviewSite.Infrastructure.Data.Identity;
+
+namespace GameReviewSite.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly IUserService service;
+
+        public UserController(
+            RoleManager<IdentityRole> _roleManager,
+            UserManager<ApplicationUser> _userManager,
+            IUserService _service)
+        {
+            roleManager = _roleManager;
+            userManager = _userManager;
+            service = _service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
