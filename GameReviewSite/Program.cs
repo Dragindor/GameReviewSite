615cc2121b72fe8a5ad5aef4ea3e1f2eee986f58
Dragindor <86678395+Dragindor@users.noreply.github.com>
2022-04-11 16:16:33 +0300

    Added contracts and services folders for futere interfaces and classes. And created one for User

--- GameReviewSite/Program.cs
+++ GameReviewSite/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-//builder.Services.AddApplicationDbContexts(builder.Configuration);
+builder.Services.AddApplicationDbContexts(builder.Configuration);
 //builder.Services.AddDefaultIdentity<ApplicationUser>(options=>
 //{
 //    options.SignIn.RequireConfirmedAccount = true;
