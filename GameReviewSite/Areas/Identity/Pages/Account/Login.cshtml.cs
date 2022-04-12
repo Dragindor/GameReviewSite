23d3f03410e9f51b2390d98ca9dc91f18db675cd
Dragindor <86678395+Dragindor@users.noreply.github.com>
2022-04-12 22:21:10 +0300

    changed from identityuser to applicationUser everywhere that was needed. added link to the accout managment.

--- GameReviewSite/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ GameReviewSite/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -15,16 +15,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Net.Mail;
+using GameReviewSite.Infrastructure.Data.Identity;
 
 namespace GameReviewSite.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger, UserManager<IdentityUser> userManager)
+        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
